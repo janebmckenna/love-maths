01e02d04be9155f1f48ef13a7332a80042756657
janebmckenna <>
2023-08-21 07:24:19 +0000

    refactor: update division game to ensure answers are intergers

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,7 @@ function runGame(gameType) {
 
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+    let num3 = num1 * num2;
 
     if (gameType === 'addition') {
         displayAdditionQuestion(num1, num2);
@@ -39,7 +40,7 @@ function runGame(gameType) {
         displaySubtractionQuestion(num1, num2);
     }
     else if (gameType === 'division') {
-        displayDivisionQuestion(num1, num2);
+        displayDivisionQuestion(num3, num2);
     }
     else {
         alert(`Unknown game type: ${gameType}`);
